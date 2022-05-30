ba4b6dec1aa43305ca8e18d5abfb5e96651f736b
KilaBash <yefancy@foxmail.com>
2022-05-30 13:53:46 +0800

    better warp effect

--- src/main/java/com/lowdragmc/shimmerfire/client/renderer/ColoredCampfireRenderer.java
+++ src/main/java/com/lowdragmc/shimmerfire/client/renderer/ColoredCampfireRenderer.java
@@ -5,22 +5,32 @@ import com.lowdragmc.shimmer.client.shader.RenderUtils;
 import com.lowdragmc.shimmerfire.CommonProxy;
 import com.lowdragmc.shimmerfire.blockentity.ColoredCampfireBlockEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Vector3f;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.Sheets;
 import net.minecraft.client.renderer.block.BlockRenderDispatcher;
+import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.NonNullList;
+import net.minecraft.util.Mth;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.CampfireBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.model.data.EmptyModelData;
 
+import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
+import java.util.Random;
 
 @OnlyIn(Dist.CLIENT)
 public class ColoredCampfireRenderer implements BlockEntityRenderer<ColoredCampfireBlockEntity> {
@@ -52,16 +62,36 @@ public class ColoredCampfireRenderer implements BlockEntityRenderer<ColoredCampf
       }
 
       pPoseStack.pushPose();
-      pPoseStack.scale(0.6f,0.8f,0.6f);
-      pPoseStack.translate(.3f,0.4,.3f);
+      pPoseStack.scale(0.7f,0.85f,0.7f);
+      pPoseStack.translate(.2f,0.4,.2f);
+      BlockPos pos = pBlockEntity.getBlockPos();
+      float finalDist = (float) Minecraft.getInstance().cameraEntity.distanceToSqr(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5);
 
       PoseStack finalStack = RenderUtils.copyPoseStack(pPoseStack);
-      pPoseStack.popPose();
       PostProcessing.WARP.postEntity(multiBufferSource -> {
          BlockRenderDispatcher brd = Minecraft.getInstance().getBlockRenderer();
-         brd.renderSingleBlock(CommonProxy.FIRE_BLOCK.get().defaultBlockState(), finalStack, multiBufferSource, pPackedLight, pPackedOverlay, EmptyModelData.INSTANCE);
+//         brd.renderSingleBlock(CommonProxy.FIRE_BLOCK.get().defaultBlockState(), finalStack, multiBufferSource, pPackedLight, pPackedOverlay, EmptyModelData.INSTANCE);
+         BlockState blockState = CommonProxy.FIRE_BLOCK.get().defaultBlockState();
+         BakedModel bakedmodel = brd.getBlockModel(blockState);
+         float scale = 0.3f * (1 - Mth.clamp(finalDist, 0, 40f) / 40f);
+         renderModel(null, finalStack.last(),
+                         multiBufferSource.getBuffer(Sheets.cutoutBlockSheet()), blockState,
+                         bakedmodel, scale, scale, scale, pPackedLight, pPackedOverlay, EmptyModelData.INSTANCE);
       });
+      pPoseStack.popPose();
+   }
 
+   public void renderModel(Direction direction, PoseStack.Pose pPose, VertexConsumer pConsumer, @Nullable BlockState pState, BakedModel pModel, float pRed, float pGreen, float pBlue, int pPackedLight, int pPackedOverlay, net.minecraftforge.client.model.data.IModelData modelData) {
+      Random random = new Random();
+      random.setSeed(42L);
+      renderQuadList(pPose, pConsumer, pRed, pGreen, pBlue, pModel.getQuads(pState, direction, random, modelData), pPackedLight, pPackedOverlay);
+
+   }
+
+   private static void renderQuadList(PoseStack.Pose pPose, VertexConsumer pConsumer, float pRed, float pGreen, float pBlue, List<BakedQuad> pQuads, int pPackedLight, int pPackedOverlay) {
+      for(BakedQuad bakedquad : pQuads) {
+         pConsumer.putBulkData(pPose, bakedquad, pRed, pGreen, pBlue, pPackedLight, pPackedOverlay);
+      }
 
    }
 }
